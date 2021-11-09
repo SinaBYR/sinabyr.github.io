f1d8b408b7c8359964fb49372d9aa5cacdf9df62
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-11-09 09:51:12 +0330

    Update axios baseURL

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -4,6 +4,7 @@ export const cancelTokenSource = axiosModule.CancelToken.source()
 
 export const axios = axiosModule.create({
     // baseURL: 'http://localhost:8000',
-    baseURL: 'https://sina-beyraghdar-portfolio-api.herokuapp.com',
+    // baseURL: 'https://sina-beyraghdar-portfolio-api.herokuapp.com',
+    baseURL: 'https://sinabyr-portfolio-api.onrender.com',
     cancelToken: cancelTokenSource.token
 })
