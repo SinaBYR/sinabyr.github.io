b226c9f9135aa9146ea93a21c642507cfd9dd706
SinaBYR <sina.beyraghdar1@gmail.com>
2022-01-26 17:25:39 +0330

    Change footer year value

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer
             className={classes.Footer}
             style={{backgroundColor: dark ? colors.dark.primary : colors.light.primary}}>
-            <p>Developed by Sina Beyraghdar © 2021</p>
+            <p>Developed by Sina Beyraghdar © {new Date().getFullYear()}</p>
             <p className={classes.Photographer}>Pian delle Betulle, Italy. Photo by <a href="https://unsplash.com/@asoggetti?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" style={{color: dark ? colors.light.primary : colors.dark.secondary}}>Alessio Soggetti</a> on <a href="https://unsplash.com/@asoggetti?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" style={{color: dark ? colors.light.primary : colors.dark.secondary}}>Unsplash.</a>
             </p>
         </footer>
